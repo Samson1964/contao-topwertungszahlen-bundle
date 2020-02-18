6600ff654246a0fd05ce33a2f8f199f20ffaad1d
Frank Hoppe <hoppe.berlin@googlemail.com>
2020-02-18 10:05:13 +0100

    Fix wegen falscher Verlinkung

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -6,7 +6,7 @@ use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
-use Schachbulle\ContaoDwzranglistenBundle\ContaoTrainerlizenzenBundle;
+use Schachbulle\ContaoDwzranglistenBundle\ContaoDwzranglistenBundle;
 
 class Plugin implements BundlePluginInterface
 {
